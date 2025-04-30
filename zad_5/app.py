5ccc0c68070f52d9e9c99edacc071d2763addafd
sasgus <sasha.e.gusev@gmail.com>
2025-04-30 22:54:08 +0300

    zad 5 gotova

---
+++ zad_5/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from models import db, Person
+from api.routes import api_bp
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Инициализация БД
+db.init_app(app)
+
+# Создаём таблицы вручную
+with app.app_context():
+    db.create_all()
+
+# Регистрация Blueprint
+app.register_blueprint(api_bp, url_prefix='/api')
+
+
+@app.route('/')
+def home():
+    people = Person.query.all()  # Теперь таблица точно существует
+    return render_template('home.html', people=people)
+
+if __name__ == '__main__':
+    app.run(debug=True)
